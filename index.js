fff266c4c31e12cb2e198a14cffebd5810833ee5
AaronSkone <81197891+AaronSkone@users.noreply.github.com>
2024-02-19 11:26:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-var http = require('http');
-var url = require('url');
-var dt = require('./datetime');
+const express = require('express');
+const app = express();
+const port = 3000; // You can change the port if needed
 
+app.use(express.static('public'));
 
-const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
-
-    // Standard Hello World.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Hello World!</h3>')
-
-    // Access funcion from a separate JavaScript module.
-    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
-
-    // Show the url. 
-    response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something tl the url so that we can parse it. 
-    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
-    var q = url.parse(request.url, true).query;
-    var txt = q.year + " " + q.month;
-    response.write("txt="+txt);
-
-    // Close the response
-    response.end('<h3>The End.</h3>');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
