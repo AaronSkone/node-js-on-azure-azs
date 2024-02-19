702320e25b52c240cb5738fdff9d95f66ff8cea3
AaronSkone <81197891+AaronSkone@users.noreply.github.com>
2024-02-19 11:27:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port if needed
+const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/roll', (req, res) => {
+    const diceResult = Math.floor(Math.random() * 6) + 1; // Server-side random number generation
+    res.json({ diceResult });
 });
 
 app.listen(port, () => {
